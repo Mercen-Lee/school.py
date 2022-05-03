c6a7af1ef7d8580325e083f28a63ce825e7afa48
이석호 <mercen@mercen.net>
2022-05-03 11:17:01 +0900

    v1.1 Update
    class update

--- school.py
+++ school.py
@@ -1,45 +1,38 @@
 '''
-school.py v1.0 developed by Seok Ho Lee
+school.py v1.1 developed by Seok Ho Lee
 (A.K.A. Mercen Lee)
-
 Github : https://github.com/Mercen-Lee
 Tistory : https://mercen.net/
 '''
-preKey = 'INSERT YOUR API KEY HERE'
+from requests import get; from re import sub
 
-from requests import get
-apiUrl = 'https://open.neis.go.kr/hub/'
-apiKey = 'Info?KEY='+preKey+'&Type=json&'
+class School:
+    def __init__(self,token):
+        self.apiUrl = 'https://open.neis.go.kr/hub/'
+        self.apiKey = 'Info?KEY='+token+'&Type=json&'
 
-def School(schoolName):
-    schoolJson = get(apiUrl+'/school'+apiKey+'SCHUL_NM='+schoolName).json()
-    schoolInfo = schoolJson['schoolInfo'][1]['row'][0]
-    schoolBase = 'ATPT_OFCDC_SC_CODE='+schoolInfo['ATPT_OFCDC_SC_CODE']
-    schoolCode = '&SD_SCHUL_CODE='+schoolInfo['SD_SCHUL_CODE']
-    return schoolBase,schoolCode
+    def Info(self,schoolName):
+        schoolJson = get(self.apiUrl+'/school'+self.apiKey+'SCHUL_NM='+schoolName).json()
+        schoolInfo = schoolJson['schoolInfo'][1]['row'][0]
+        schoolBase = 'ATPT_OFCDC_SC_CODE='+schoolInfo['ATPT_OFCDC_SC_CODE']
+        schoolCode = '&SD_SCHUL_CODE='+schoolInfo['SD_SCHUL_CODE']
+        return schoolBase,schoolCode
 
-def Meal(schoolName,mealCode,mealDate):
-    schoolBase,schoolCode = School(schoolName)
-    mealName = '&MLSV_YMD='+str(mealDate)+'&MMEAL_SC_CODE='+str(mealCode)
-    mealJson = get(apiUrl+'mealServiceDiet'+apiKey+schoolBase+schoolCode+mealName).json()
-    return mealJson['mealServiceDietInfo'][1]['row'][0]['DDISH_NM'].replace('<br/>','\n')
+    def Class(self,schoolName,schoolGrade,schoolClass,classDate):
+        schoolBase,schoolCode = self.Info(schoolName)
+        if schoolName.endswith('초등학교'): schoolLevel = 'elsTimetable'
+        elif schoolName.endswith('중학교'): schoolLevel = 'misTimetable'
+        elif schoolName.endswith('고등학교'): schoolLevel = 'hisTimetable'
+        className = '&GRADE='+str(schoolGrade)+'&CLASS_NM='+str(schoolClass)+'&ALL_TI_YMD='+str(classDate)
+        classJson = get(self.apiUrl+schoolLevel+self.apiKey[4:]+schoolBase+schoolCode+className).json()
+        classBase = classJson[schoolLevel][1]['row']; classList = []
+        for x in classBase: classList.append(sub('[-,:.*]','',x['ITRT_CNTNT']).strip())
+        return classList
 
-def classCrawl(schoolName,schoolGrade,schoolClass,classDate):
-    schoolBase,schoolCode = School(schoolName)
-    if schoolName.endswith('초등학교'): schoolLevel = 'elsTimetable'
-    elif schoolName.endswith('중학교'): schoolLevel = 'misTimetable'
-    elif schoolName.endswith('고등학교'): schoolLevel = 'hisTimetable'
-    className = '&GRADE='+str(schoolGrade)+'&CLASS_NM='+str(schoolClass)+'&ALL_TI_YMD='+str(classDate)
-    classJson = get(apiUrl+schoolLevel+apiKey[4:]+schoolBase+schoolCode+className).json()
-    return classJson[schoolLevel][1]['row']
-
-def Class(schoolName,schoolGrade,schoolClass,classTime,classDate):
-    classJson = classCrawl(schoolName,schoolGrade,schoolClass,classDate)
-    return classJson[classTime-1]['ITRT_CNTNT'].replace('-','')
-
-def ClassAll(schoolName,schoolGrade,schoolClass,classDate):
-    classJson = classCrawl(schoolName,schoolGrade,schoolClass,classDate); classList = ''
-    for i in range(20):
-        try: classList += str(i+1)+'교시 - '+classJson[i]['ITRT_CNTNT'].replace('-','')+'\n'
-        except: break
-    return classList[:-1]
+    def Meal(self,schoolName,mealCode,mealDate):
+        schoolBase,schoolCode = self.Info(schoolName)
+        mealName = '&MLSV_YMD='+str(mealDate)+'&MMEAL_SC_CODE='+str(mealCode)
+        mealJson = get(self.apiUrl+'mealServiceDiet'+self.apiKey+schoolBase+schoolCode+mealName).json()
+        mealBase = mealJson['mealServiceDietInfo'][1]['row'][0]['DDISH_NM'].split('<br/>'); mealList = []
+        for x in mealBase: mealList.append(sub('[-,:.* ]','',x).split('(')[0])
+        return mealList
