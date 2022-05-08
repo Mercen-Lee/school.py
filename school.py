8b63dd681ae9fd00ee74d4af4e28246f6b28aa0d
이석호 <mercen@mercen.net>
2022-05-08 14:29:53 +0900

    v1.3 Bugfix

--- school.py
+++ school.py
@@ -17,7 +17,8 @@ class School:
         infoLink = self.apiUrl+'/school'+self.apiKey+'SCHUL_NM='+quote(self.Name)
         infoJson = loads(urlopen(infoLink).read())['schoolInfo'][1]['row'][0]
         infoBase = 'ATPT_OFCDC_SC_CODE='+infoJson['ATPT_OFCDC_SC_CODE']
-        infoCode = '&SD_SCHUL_CODE='+infoJson['SD_SCHUL_CODE']; return infoBase,infoCode
+        infoCode = '&SD_SCHUL_CODE='+infoJson['SD_SCHUL_CODE'];
+        return infoBase,infoCode
 
     def Timetable(self):
         infoBase,infoCode = self.Info()
@@ -27,11 +28,13 @@ class School:
         timeName = '&GRADE='+str(self.Grade)+'&CLASS_NM='+str(self.Class)+'&ALL_TI_YMD='+str(self.Date)
         timeLink = self.apiUrl+schoolLevel+self.apiKey[4:]+infoBase+infoCode+timeName
         timeJson = loads(urlopen(timeLink).read())[schoolLevel][1]['row']; timeTable = []
-        for x in timeJson: timeTable.append(sub('[-,:.*]','',x['ITRT_CNTNT']).strip()); return timeTable
+        for x in timeJson: timeTable.append(sub('[-,:.*]','',x['ITRT_CNTNT']).strip())
+        return timeTable
 
     def Meals(self):
         infoBase,infoCode = self.Info()
         mealName = '&MLSV_YMD='+str(self.Date)+'&MMEAL_SC_CODE='+str(self.Meal)
         mealLink = self.apiUrl+'mealServiceDiet'+self.apiKey+infoBase+infoCode+mealName; meals = []
         mealJson = loads(urlopen(mealLink).read())['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
-        for x in mealJson.split('<br/>'): meals.append(sub('[-,:.* ]','',x).split('(')[0]); return meals
+        for x in mealJson.split('<br/>'): meals.append(sub('[-,:.* ]','',x).split('(')[0])
+        return meals
